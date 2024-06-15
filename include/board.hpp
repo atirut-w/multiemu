033dbc41153c74d0bd8858a74385507905f53d03
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-06-15 22:51:42 +0700

    Add note on board meta

--- include/board.hpp
+++ include/board.hpp
@@ -7,6 +7,7 @@ struct Board
     virtual ~Board() = default;
 };
 
+// Dev note: Please do not put anything that modifies itself in board meta, as it will be placed in a read-only section
 struct BoardMeta
 {
     const char *name;
