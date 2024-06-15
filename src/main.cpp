7ed60dc549b693a877837ea28059bae340f9e285
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-06-15 20:40:27 +0700

    Add Argparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
+#include <memory>
 
-int main()
+using namespace std;
+using namespace argparse;
+
+unique_ptr<const ArgumentParser> parse_arguments(int argc, const char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    auto parser = make_unique<ArgumentParser>("Multi-machine emulator");
+
+    try
+    {
+        parser->parse_args(argc, argv);
+        return parser;
+    }
+    catch (const exception &error)
+    {
+        cerr << error.what() << endl;
+        cerr << *parser;
+        exit(1);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    auto args = parse_arguments(argc, argv);
+
     return 0;
 }
