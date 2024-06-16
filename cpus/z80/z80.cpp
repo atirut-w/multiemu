6d7102ab8e6210f5d2cc07458f851c2648af0a39
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-06-16 19:31:48 +0700

    Z80: Extract opcode fields

--- cpus/z80/z80.cpp
+++ cpus/z80/z80.cpp
@@ -8,4 +8,9 @@ uint8_t Z80::fetch()
 void Z80::run_instruction()
 {
     uint8_t opcode = fetch();
+    uint8_t x = (opcode & 0b11000000) >> 6;
+    uint8_t y = (opcode & 0b00111000) >> 3;
+    uint8_t z = opcode & 0b00000111;
+    uint8_t p = (y & 0b110) >> 1;
+    uint8_t q = y & 0b001;
 }
