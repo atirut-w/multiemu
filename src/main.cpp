6786522919ba732ee3200123b22b3967117f85b4
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-23 00:07:23 +0700

    implement board listing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <argparse/argparse.hpp>
+#include <board_registry.hpp>
 #include <iostream>
 #include <memory>
+#include <multiemu/board.hpp>
 
 using namespace std;
 using namespace argparse;
@@ -8,7 +10,13 @@ using namespace argparse;
 unique_ptr<const ArgumentParser> parse_arguments(int argc, const char *argv[]) {
   auto parser = make_unique<ArgumentParser>("Multi-machine emulator");
 
-  parser->add_argument("-b", "--board").help("Board to emulate").required();
+  auto &mutex = parser->add_mutually_exclusive_group(true);
+
+  // Board to emulate
+  mutex.add_argument("board").help("Board to emulate").nargs(1);
+
+  // List available boards
+  mutex.add_argument("-l", "--list").help("List available boards").default_value(false).implicit_value(true);
 
   try {
     parser->parse_args(argc, argv);
@@ -24,6 +32,15 @@ int main(int argc, const char *argv[]) {
   auto args = parse_arguments(argc, argv);
   if (!args) {
     return 1;
+  }
+
+  if (args->get<bool>("--list")) {
+    cout << "Available boards:" << endl;
+    for (const auto *board : BoardRegistry::get_board_infos()) {
+      cout << "\t" << board->name << endl;
+    }
+
+    return 0;
   }
 
   return 0;
