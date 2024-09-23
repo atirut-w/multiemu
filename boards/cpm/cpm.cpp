d724dffad3bf1b2e7b1290e76c9610218a55bf71
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-23 13:02:20 +0700

    CP/M: hook up memory read/write

--- boards/cpm/cpm.cpp
+++ boards/cpm/cpm.cpp
@@ -1,10 +1,21 @@
 #include "cpm.hpp"
+#include <cstddef>
+#include <iostream>
 #include <memory>
 
 using namespace std;
 
 unique_ptr<Board> create_cpm() {
   return make_unique<CPMBoard>();
+}
+
+CPMBoard::CPMBoard() {
+  cpu.read = [this](size_t address) {
+    return memory[address];
+  };
+  cpu.write = [this](size_t address, uint8_t value) {
+    memory[address] = value;
+  };
 }
 
 int CPMBoard::step() {
--- boards/cpm/cpm.hpp
+++ boards/cpm/cpm.hpp
@@ -1,9 +1,15 @@
 #pragma once
 #include "multiemu/board.hpp"
 #include "z80/z80.hpp"
+#include <array>
+#include <cstdint>
 
 class CPMBoard : public Board {
   Z80 cpu;
+  std::array<uint8_t, 0x10000> memory;
 
   virtual int step() override;
+
+public:
+  CPMBoard();
 };
