f0cef586b2659df182235aabda609f51a4dcda85
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-23 12:55:20 +0700

    Wrap necessary Z80 functions

--- cpus/z80/z80.cpp
+++ cpus/z80/z80.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <cstring>
 
 using namespace std;
@@ -13,8 +14,37 @@ Wrapped::Z80 *get_cpu(void *cpu) {
   return static_cast<Wrapped::Z80 *>(cpu);
 }
 
+Z80 *get_self(void *self) {
+  return static_cast<Z80 *>(self);
+}
+
+uint8_t readw(void *ctx, uint16_t address) {
+  return get_self(ctx)->read(address);
+}
+
+void writew(void *ctx, uint16_t address, uint8_t value) {
+  get_self(ctx)->write(address, value);
+}
+
+uint8_t inw(void *ctx, uint16_t port) {
+  return get_self(ctx)->in(port);
+}
+
+void outw(void *ctx, uint16_t port, uint8_t value) {
+  get_self(ctx)->out(port, value);
+}
+
 Z80::Z80() {
   Wrapped::Z80 *cpu = new Wrapped::Z80();
+  cpu->context = this;
+
+  cpu->fetch_opcode = readw;
+  cpu->fetch = readw;
+  cpu->read = readw;
+  cpu->write = writew;
+  cpu->in = inw;
+  cpu->out = outw;
+
   real_cpu = cpu;
 }
 
---
+++ include/multiemu/io.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+
+struct IO {
+  std::function<uint8_t(size_t)> in = [](size_t) { return 0; };
+  std::function<void(size_t, uint8_t)> out = [](size_t, uint8_t) {};
+};
