e20b434592e45aa66e5dee0a4826752d806f8614
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-23 10:56:23 +0700

    Wrap external Z80 emulator

--- cpus/z80/z80.cpp
+++ cpus/z80/z80.cpp
@@ -1,5 +1,27 @@
+#include <cstring>
+
+using namespace std;
+
+namespace Wrapped {
+  extern "C" {
+    #include <Z80.h>
+  }
+}
 #include "z80/z80.hpp"
 
+Wrapped::Z80 *get_cpu(void *cpu) {
+  return static_cast<Wrapped::Z80 *>(cpu);
+}
+
+Z80::Z80() {
+  Wrapped::Z80 *cpu = new Wrapped::Z80();
+  real_cpu = cpu;
+}
+
+Z80::~Z80() {
+  delete static_cast<Wrapped::Z80 *>(real_cpu);
+}
+
 int Z80::step() {
-  return 0;
+  return Wrapped::z80_run(get_cpu(real_cpu), 1);
 }
--- cpus/z80/z80.hpp
+++ cpus/z80/z80.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include "multiemu/cpu.hpp"
+#include <memory>
 
 class Z80 : public CPU {
+  void *real_cpu;
+
 public:
+  Z80();
+  ~Z80();
   int step() override;
 };
