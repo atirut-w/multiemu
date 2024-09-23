0f21cd59ad666baf883119b78e624d92f4256894
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-23 20:00:37 +0700

    Slim down base CPU class

--- include/multiemu/cpu.hpp
+++ include/multiemu/cpu.hpp
@@ -7,10 +7,8 @@ using ReadCallback = std::function<uint8_t(size_t)>;
 using WriteCallback = std::function<void(size_t, uint8_t)>;
 
 struct CPU {
-  ReadCallback fetch_opcode = [](size_t) { return 0; };
   ReadCallback read = [](size_t) { return 0; };
   WriteCallback write = [](size_t, uint8_t) {};
 
   virtual int step() = 0;
-  virtual void set_trap(size_t opcode, std::function<size_t(size_t)> handler) {}
 };
