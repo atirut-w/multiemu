985a032ba7aea1cd616a6c0dadac18d1dcf96c31
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-27 11:00:55 +0700

    Give bus devices a default offset

--- include/multiemu/bus_device.hpp
+++ include/multiemu/bus_device.hpp
@@ -2,7 +2,7 @@
 #include <cstddef>
 
 template <typename DataType> struct BusDevice {
-  std::size_t offset;
+  std::size_t offset = 0;
   virtual DataType read(std::size_t addr) { return 0; }
   virtual void write(std::size_t addr, DataType data) {}
 };
