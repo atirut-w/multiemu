b396f76a6b84abd19ae95b8de0407b336fc66b52
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-28 15:55:24 +0700

    Fantacom: update boot ROM

--- boards/fantacom/bootrom/rt0.s
+++ boards/fantacom/bootrom/rt0.s
@@ -5,11 +5,20 @@ _start:
     ld hl, charset
 
     out (c), l
-    inc c
+    inc bc
     out (c), h
-    inc c
+    inc bc
     xor a
     out (c), a
+
+    ; ld hl, hello
+    ; inc bc
+    ; out (c), l
+    ; inc bc
+    ; out (c), h
+    ; inc bc
+    ; xor a
+    ; out (c), a
     jp _hang
 
 
@@ -18,3 +27,8 @@ _start:
 _hang:
     halt
     jp _hang
+
+
+    .section .data
+hello:
+    .db 'H', 0x07, 'e', 0x07, 'l', 0x07, 'l', 0x07, 'o', 0x07, '!', 0x07, 0x00
