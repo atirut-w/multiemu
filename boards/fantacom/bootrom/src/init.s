f8ad51fa23a143b3ab40bdfdfae7b17342b3d377
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-12 12:45:17 +0700

    boot ROM: correct MMU init

--- boards/fantacom/bootrom/src/init.s
+++ boards/fantacom/bootrom/src/init.s
@@ -1,12 +1,13 @@
+    .section .startup
     .equ PAGETABLE, 0
-    .equ TEST_AREA, 0x2000
     .equ START_PAGE, 2
+    .equ VIRT_PAGE, 4
     
     .global _start
 _start:
-    ld bc, (PAGETABLE+2)
-    ld hl, TEST_AREA
-    ld d, START_PAGE
+    ld bc, PAGETABLE + VIRT_PAGE ; Virtual-to-physical remap registers
+    ld hl, VIRT_PAGE << 12 ; Virtual page address
+    ld d, START_PAGE ; First physical page to test
 0:
     out (c), d
     ld (hl), 0x55
@@ -16,10 +17,13 @@ _start:
 
     inc d
     jr nz, 0b
-1:
-    ; We are cooked
-    halt
-    jr 1b
+    jr _hang
 2:
-    ; jp _crt0_start
-    ret
+    ld hl, VIRT_PAGE + 0x1000
+    ld sp, hl
+    jp _crt0_start
+    jr _hang
+
+_hang:
+    halt
+    jr _hang
