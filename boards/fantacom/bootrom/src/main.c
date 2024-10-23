28cc3f5e818a2842ac6ff8fa46fa75174e50d252
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-23 17:34:22 +0700

    fantacom: bootrom: test vga

--- boards/fantacom/bootrom/src/main.c
+++ boards/fantacom/bootrom/src/main.c
@@ -2,8 +2,35 @@
 #include <stdint.h>
 
 extern char font;
+const uint8_t vga_data[] = {
+  'H',
+  0x07,
+  'e',
+  0x07,
+  'l',
+  0x07,
+  'l',
+  0x07,
+  'o',
+  0x07,
+  ' ',
+  0x07,
+  'W',
+  0x07,
+  'o',
+  0x07,
+  'r',
+  0x07,
+  'l',
+  0x07,
+  'd',
+  0x07,
+  '!',
+  0x07,
+};
 
 int main() {
   outl(16, (uint32_t)&font & 0xffff);
+  outl(20, (uint32_t)&vga_data & 0xffff);
   return 0;
 }
