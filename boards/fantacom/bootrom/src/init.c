9f933d23430a8204b6a6a1ced13a2a07a3b8dc49
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-11-04 19:09:23 +0700

    fantacom: bootrom: optimize init

--- boards/fantacom/bootrom/src/init.c
+++ boards/fantacom/bootrom/src/init.c
@@ -1,35 +1,35 @@
+#include <string.h>
+
 typedef struct DriverInfo {
   void (*init)(void);
 } DriverInfo;
 
-extern char data_start;
-extern char data_end;
-extern char data_load;
+extern char data_start[];
+extern char data_end[];
+extern char data_load[];
 
-extern char bss_start;
-extern char bss_end;
+extern char bss_start[];
+extern char bss_end[];
 
-extern DriverInfo drivers_start;
-extern DriverInfo drivers_end;
+extern DriverInfo drivers_start[];
+extern DriverInfo drivers_end[];
 
 void init_data() {
-  char *src = &data_load;
-  char *dst = &data_start;
-  while (dst < &data_end) {
-    *dst++ = *src++;
-  }
+  char *src = data_load;
+  char *dst = data_start;
+  memcpy(dst, src, data_end - data_start);
 }
 
 void init_bss() {
-  char *dst = &bss_start;
-  while (dst < &bss_end) {
+  char *dst = bss_start;
+  while (dst < bss_end) {
     *dst++ = 0;
   }
 }
 
 void init_drivers() {
-  DriverInfo *driver = &drivers_start;
-  while (driver < &drivers_end) {
+  DriverInfo *driver = drivers_start;
+  while (driver < drivers_end) {
     driver->init();
     driver++;
   }
