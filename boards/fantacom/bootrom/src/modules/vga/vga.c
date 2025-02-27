3593e96648c6aaac05fc717bb8ff1600efde4726
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-27 21:36:55 +0700

    Fantacom: boot ROM: fix incorrect VGA init

--- boards/fantacom/bootrom/src/modules/vga/vga.c
+++ boards/fantacom/bootrom/src/modules/vga/vga.c
@@ -1,6 +1,9 @@
 #include "io.h"
 #include <stdint.h>
+#include "mmu/mmu.h"
 
+#define WIDTH 80
+#define HEIGHT 25
 #define CHARSET_ADDR_PORT 16
 #define SCREEN_ADDR_PORT 20
 
@@ -10,9 +13,10 @@ typedef struct {
 } VGAChar;
 
 extern char font[];
-VGAChar vram[80 * 25];
+VGAChar vram[WIDTH * HEIGHT];
+int cursor = 0;
 
 __attribute__((constructor)) void vga_init(void) {
-  out32(CHARSET_ADDR_PORT, (uint32_t)font & 0xffff);
-  out32(SCREEN_ADDR_PORT, (uint32_t)vram & 0xffff);
+  out32(CHARSET_ADDR_PORT, mmu_get_physical(font));
+  out32(SCREEN_ADDR_PORT, mmu_get_physical(vram));
 }
