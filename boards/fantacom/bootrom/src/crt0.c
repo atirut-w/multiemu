599c74eeb9348a0ba01f4da471ccc2732b0e3091
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-27 13:21:04 +0700

    Fantacom: boot ROM: implement CRT0

--- boards/fantacom/bootrom/src/crt0.c
+++ boards/fantacom/bootrom/src/crt0.c
@@ -1,1 +1,18 @@
-void start(void) {}
+#include <string.h>
+
+extern char __data_start[], __data_end[], __data_load[];
+extern char __bss_start[], __bss_end[];
+
+void __libc_start_main(int (*main)(int, char **, char **), int argc, char **argv, void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void *stack_end);
+int main(int argc, char **argv, char **env);
+
+void start(void) {
+  memcpy(__data_start, __data_load, __data_end - __data_start);
+  memset(__bss_start, 0, __bss_end - __bss_start);
+
+  __libc_start_main(main, 0, 0, 0, 0, 0, 0);
+}
+
+void _exit(int status) {
+  while (1);
+}
