548c16f26ed8a57b187bb1f2be17ef48a8419721
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-04-28 00:38:27 +0700

    Add ImGui stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <raylib.h>
+#include <rlImGui.h>
+#include <imgui.h>
 
 int main()
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(1280, 720, "MultiEmu");
     SetTargetFPS(60);
+    rlImGuiSetup(true);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(GRAY);
+
+        rlImGuiBegin();
+        rlImGuiEnd();
+
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow(); // Close window and OpenGL context
 }
