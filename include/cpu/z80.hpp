77d85c37130b170e385542ad23ca3f6e7ab31f9a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-06-26 12:07:05 +0700

    Migrate to use Chips Z80 core

--- include/cpu/z80.hpp
+++ include/cpu/z80.hpp
@@ -1,25 +1,17 @@
 #include "multiemu/cpu.hpp"
+#include <cstdint>
 #include <memory>
+#include <z80.h>
 
 namespace MultiEmu {
 
 class Z80 : public CPU {
 private:
-  // Opaque pointer to implementation
-  struct Impl;
-  std::unique_ptr<Impl> pImpl;
+  z80_t cpu;
+  std::uint64_t pins = 0;
 
 public:
   Z80();
-  virtual ~Z80();
-
-  // Prevent copying
-  Z80(const Z80 &) = delete;
-  Z80 &operator=(const Z80 &) = delete;
-
-  // Allow moving
-  Z80(Z80 &&) noexcept;
-  Z80 &operator=(Z80 &&) noexcept;
 
   // Core execution
   virtual int execute(int cycles) override;
@@ -38,7 +30,7 @@ public:
 
   // Program counter access for memory view positioning
   virtual size_t getProgramCounter() const override;
-  
+
   // Interrupt interface
   virtual void requestInterrupt(uint32_t vector = 0, bool nmi = false) override;
   virtual bool areInterruptsEnabled() const override;
